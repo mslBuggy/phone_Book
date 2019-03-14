923c5cc21e09a50d87919a6b6b0724cb7c98ed61
Manuel Lara Bisch <manuel.lara.bisch@accenture.com>
2019-03-14 23:17:32 +0100

    Interactive mode now has its own private method, and instructions to enter it

--- src/com/acn/book/phone/LookupInfo.java
+++ src/com/acn/book/phone/LookupInfo.java
@@ -8,27 +8,78 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LookupInfo {
-	
+
 	private String addressFromName(String input, Map<String, Person> personMap) {
 		if (personMap.get(input).getAddress() == null)
 			return "No address found for this person";
-		return personMap.get(input).getAddress();
+		return "Address: " + personMap.get(input).getAddress();
 	}
-	
+
 	private String phoneFromName(String input, Map<String, Person> personMap) {
 		Person p = personMap.get(input);
 		if (p.getPhoneNumber() == null) {
 			return "No phone number found for this person";
 		}
-		return p.getPhoneNumber();
+		return "Phone: " + p.getPhoneNumber();
 	}
 
-	private void Logic(Map<String, Person> book) throws IOException {
+	private Map<String, Person> interact(Map<String, Person> personMap, BufferedReader br, boolean xyzzy) throws IOException {
+		System.out.println("Interactive mode enabled.");
+		PhoneBook pb = new PhoneBook(personMap);
+		String exit;
+		while (true) {
+			if (xyzzy) {
+				System.out.println("Enter 1 to add an entry, 2 to remove. PLUGH exits.");
+				exit = "PLUGH";
+			} else {
+				System.out.println("Enter 1 to add an entry, 2 to remove.\nEnter 9 to return to the standard name search.");
+				exit = "9";
+			}
+			String sIn = br.readLine();
+			if (sIn.equals(exit)) {
+				System.out.println("Quick name search mode restored.");
+				break;
+			}
+			if (sIn.equals("2")) {
+				System.out.println("Enter name of the entry to delete");
+				sIn = br.readLine();
+				pb.deleteEntry(sIn);
+				personMap.remove(sIn);
+			} else if (sIn.equals("1")) {
+				System.out.println("Enter name of new entry");
+				sIn = br.readLine();
+				if (personMap.containsKey(sIn)) {
+					// Key already exists
+					System.out.println("Person with this name already exists. New entry cannot be added");
+				} else if (sIn.equals("")) {
+					System.out.println("Name field cannot be empty.");
+				} else {
+					Person p = new Person(sIn, null, null);
+					System.out.println("Enter address of new entry, or press enter to skip.");
+					sIn = br.readLine();
+					if (!sIn.equals(""))
+						p.setAddress(sIn);
+					System.out.println("Enter phone number of new entry, or press enter to skip.");
+					sIn = br.readLine();
+					if (!sIn.equals(""))
+						p.setPhoneNumber(sIn);
+					pb.addEntry(p);
+					personMap.put(p.getName(), p);
+//					System.out.println(personMap);
+				}
+			}
+		}
+		return personMap;
+
+	}
+
+	private void businessLogic(Map<String, Person> book) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		// TODO start app in console with "Name:" prompt, wait for input
 		PROMPT: while (true) {
 			System.out.println(
-					"Welcome to the phone book application.\nType a name to search for an entry, or type \"closeapp\" to exit.\t\t\t\t\t\t\t\t\t\txyzzy");
+					"\nWelcome to the phone book application.\nType a name to search for an entry, or type \"closeapp\" to exit.\n"
+							+ "Or type \"interact\" to open interactive mode for adding or removing entries.");
 			System.out.print("Name: ");
 			String sIn = in.readLine();
 
@@ -42,44 +93,12 @@ public class LookupInfo {
 			 * TODO 1. if contact not existent, print error message
 			 */
 			if (sIn.equals("xyzzy")) {
-				System.out.println("A hollow voice says \"PLUGH\".\nInteractive mode enabled.");
-				PhoneBook pb = new PhoneBook(book);
-				while (true) {
-					System.out.println("Enter 1 to add an entry, 2 to remove. PLUGH exits.");
-					sIn = in.readLine();
-					if (sIn.equals("PLUGH")) {
-						System.out.println("Normal function restored.");
-						continue PROMPT;
-					}
-					if (sIn.equals("2")) {
-						System.out.println("Enter name of the entry to delete");
-						sIn = in.readLine();
-						pb.deleteEntry(sIn);
-						book.remove(sIn);
-					} else if (sIn.equals("1")) {
-						System.out.println("Enter name of new entry");
-						sIn = in.readLine();
-						if (book.containsKey(sIn)) {
-							// Key already exists
-							System.out.println("Person with this name already exists. New entry cannot be added");
-						} else if (sIn.equals("")) {
-							System.out.println("Name field cannot be empty.");
-						} else {
-							Person p = new Person(sIn, null, null);
-							System.out.println("Enter address of new entry, or press enter to skip.");
-							sIn = in.readLine();
-							if (!sIn.equals(""))
-								p.setAddress(sIn);
-							System.out.println("Enter phone number of new entry, or press enter to skip.");
-							sIn = in.readLine();
-							if (!sIn.equals(""))
-								p.setPhoneNumber(sIn);
-							pb.addEntry(p);
-							book.put(p.getName(), p);
-							System.out.println(book);
-						}
-					}
-				}
+				System.out.println("A hollow voice says \"PLUGH\".");
+				book = interact(book, in, true);
+				continue PROMPT;
+			}else if (sIn.equals("interact")) {
+				book = interact(book, in, false);
+				continue PROMPT;
 			}
 			if (!book.containsKey(sIn)) {
 				System.out.println("No such person found in phone book.");
@@ -88,7 +107,7 @@ public class LookupInfo {
 				 * TODO 2. search Address: if found, print contact address else
 				 * "no address saved for Person"
 				 */
-				
+
 				System.out.println(addressFromName(sIn, book));
 				/*
 				 * TODO 3. search phone#: if found, print phone# else, printout
@@ -107,14 +126,14 @@ public class LookupInfo {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO initialize map
+		// initialize map
 		Map<String, Person> pBook = new HashMap<>();
 		String name1 = "Manuel", name2 = "Thuong", name3 = "Ali";
 		pBook.put(name1, new Person(name1, "Adickesallee 65, Frankfurt", "0172-45533877"));
 		pBook.put(name2, new Person(name2, null, "0176-2134799"));
 		pBook.put(name3, new Person(name3, "Adickesallee 43, Frankfurt", null));
 		LookupInfo li = new LookupInfo();
-		li.Logic(pBook);
+		li.businessLogic(pBook);
 		System.out.println("Goodbye.");
 		System.exit(0);
 	}
