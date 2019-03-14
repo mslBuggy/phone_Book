71ef2f44b81e9dc46d984cd1f74c80298e56c385
Manuel Lara Bisch <manuel.lara.bisch@accenture.com>
2019-03-14 15:04:49 +0100

    mocks for search functionality

--- src/com/acn/book/phone/LookupInfo.java
+++ src/com/acn/book/phone/LookupInfo.java
@@ -1,12 +1,16 @@
 package com.acn.book.phone;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LookupInfo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		// TODO initialize map
 		Map<String, Person> pBook = new HashMap<>();
 		String name1 = "Manuel", name2 = "Thuong", name3 = "Ali";
@@ -14,26 +18,39 @@ public class LookupInfo {
 		pBook.put(name2, new Person(name2, null, "0176-2134799"));
 		pBook.put(name1, new Person(name3, "Adickesallee 43, Frankfurt", null));
 		// TODO start app in console with "Name:" prompt, wait for input
-		System.out.print("Name: ");
-		// TODO after input with Enter:
-		/*
-		 * 1. if contact not existent, print error message
-		 */
-		
-		/* TODO
-		 * 2. search Address:
-		 * if found, print contact address
-		 * else "no address saved for Person"
-		 */
-		
-		/* TODO
-		 * 3. search phone#: if found, print phone#
-		 * else, printout "no phone number saved for Person"
-		 */
-		
-		// TODO 4. print prompt again, wit for input
-		
-		// TODO if input: "closeapp", end app
+		while (true) {
+
+			System.out.print("Name: ");
+			String sIn = in.readLine();
+
+			// TODO after input with Enter:
+			/*
+			 * 1. if contact not existent, print error message
+			 */
+			if (!pBook.containsKey(sIn)) {
+				System.out.println("No such person found in phone book.");
+			} else {
+				/* TODO
+				 * 2. search Address:
+				 * if found, print contact address
+				 * else "no address saved for Person"
+				 */
+				System.out.println("Hallo " + sIn);
+				/* TODO
+				 * 3. search phone#: if found, print phone#
+				 * else, printout "no phone number saved for Person"
+				 */
+				System.out.println("Nummer " + sIn);
+
+			// TODO 4. print prompt again, wait for input
+			}
+
+			// TODO if input: "closeapp", end app
+			if (sIn.equalsIgnoreCase("closeapp")){
+				break;
+			}
+		}
+		in.close();
 
 	}
 
