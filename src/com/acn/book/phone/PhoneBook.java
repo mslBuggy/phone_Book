9148f7500a5f329e8ce3c2e4b6f16ce3e8107334
Manuel Lara Bisch <manuel.lara.bisch@accenture.com>
2019-03-14 14:17:12 +0100

    Java

---
+++ src/com/acn/book/phone/PhoneBook.java
@@ -0,0 +1,24 @@
+package com.acn.book.phone;
+
+/**
+ * Create a phonebook
+ * A phonebook has many entries
+ * An entry consists of
+ * 		Name
+ * 		Address
+ * 		Phone no.
+ * This phonebook has basic search functions using name as parameter
+ * 	Search for address (for Developer 1
+ *  Search for phone number (for dev 2)
+ *  Additional functions add-phonebook-entry & delete-phonebook-entry (for dev 3)
+ * Combine the implementations in git
+ * @author manuel.lara.bisch
+ *
+ */
+public class PhoneBook {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+}
